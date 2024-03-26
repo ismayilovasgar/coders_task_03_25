9e872f021c7b24b14cef3844e738725f8bd9d606
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-03-26 10:55:54 +0400

    insert

---
+++ app.js
@@ -0,0 +1,26 @@
+// Task - 25.03.2024
+// 1.İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.
+// let txt_1=prompt("1-) bir soz daxil edin:");
+// console.log("length:",txt_1.length);
+
+// 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
+// let txt_2 = prompt("2-) bir soz daxil edin:");
+// let txt_2_simvol = prompt("bir simvol daxil edin");
+// console.log(txt_2.includes(txt_2_simvol));
+
+// 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+// let txt_3 = prompt("3-) bir soz daxil edin:");
+// console.log(txt_3.startsWith(txt_3.charAt(1)));
+
+// 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+// let txt_4 = prompt("4-) bir soz daxil edin:");
+// console.log(txt_4.endsWith(txt_4.charAt(1)));
+
+// 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
+// let txt_5=prompt("5-) bir soz daxil edin:")
+// console.log(txt_5.charCodeAt(1));
+
+// 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
+// let txt_6 = prompt("6-) bir soz daxil edin:");
+// console.log(txt_6.includes(txt_6.toUpperCase()));
+
